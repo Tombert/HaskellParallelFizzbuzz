4bdcc86f69df222e240ed5c8df3da3b9f55b1dd3
Thomas Gebert <tombert@linux-r8xk.site>
2014-09-19 20:34:23 -0500

    Make initial (and probably only) commit to make sure file is on github. :metal:

---
+++ main.hs
@@ -0,0 +1,36 @@
+import Control.Parallel (par)
+
+-- This is effectively a reimplementation of the "map" function, but 
+-- we get to utilize Haskell's neat "sparks", thus allowing for fast and efficient
+-- multithreading, mostly handled by the runtime. 
+-- Copied from https://github.com/cyga/real-world-haskell/blob/master/ch24/ParMap.hs 
+parMap :: ( a -> b) -> [a] -> [b]
+parMap f (x:xs) = let r = f x
+                  in r `par` r : parMap f xs
+parMap _ _ = []
+
+
+fizzBuzz :: (Integral a, Show a) => a -> String
+fizzBuzz x | x `mod` 3 == 0 && x `mod` 5 == 0 = "FizzBuzz"
+           | x `mod` 3 == 0 = "Fizz"
+           | x `mod` 5 == 0 = "Buzz"
+           | otherwise = show x
+
+-- We'll partially apply the fizzBuzz function to quickly 
+-- generate a helper function. 
+parallelFizzMap = parMap fizzBuzz
+
+-- This is just a quick helper function 
+-- to concat two strings together. 
+concatWithComma :: String -> String -> String
+concatWithComma x y = x ++ "," ++ y
+
+-- Again, using partial application, we can 
+-- quickly generate a helper function. 
+-- 
+-- The "" at the end is there to give some
+-- initial value and to make the types line up. 
+makeList = foldr concatWithComma ""
+
+main :: IO ()
+main = putStrLn $ makeList . parallelFizzMap $ [1..100]
